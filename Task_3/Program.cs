b0234e741cd4252f770362a659647a31098d599b
DenniSergeich <damned_wolf@mail.ru>
2023-01-27 16:17:15 +0300

    Task 3

---
+++ Task_3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+if (number%2==0){
+    Console.Write($"Число {number} чётное.");
+}
+else {
+    Console.Write($"Число {number} нечётное.");
+}
