a3b40a2f06c4302ef4b3878cc72754ded10d9e5d
DenniSergeich <damned_wolf@mail.ru>
2023-01-27 16:06:37 +0300

    Task 1

---
+++ Task_1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int numberOne = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberTwo = int.Parse(Console.ReadLine());
+if (numberOne > numberTwo)
+{
+    Console.WriteLine($"Число {numberOne} большее, а число {numberTwo} меньшее.");
+}
+else
+{
+    Console.WriteLine($"Число {numberTwo} большее, а число {numberOne} меньшее.");
+}
