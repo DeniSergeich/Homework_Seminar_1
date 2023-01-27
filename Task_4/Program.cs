d639897c0773cd309e80de43ce96c85257aab810
DenniSergeich <damned_wolf@mail.ru>
2023-01-27 16:34:01 +0300

    Task 4

---
+++ Task_4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите число > 1: ");
+int number = int.Parse(Console.ReadLine());
+int i = 2;
+if (number <= 0)
+{
+    Console.Write("Вы ввели некорректное число!");
+    return;
+}
+while (i <= number)
+{
+    Console.Write($"{i} ");
+    i += 2;
+}
